28f360f018e43754425aa0174a5ea9c21976a933
Jon Martin <jchristofmartin@gmail.com>
2017-12-09 15:56:16 -0600

    adding an expression tree visualizer

--- Expression Tree/MainWindow.xaml.cs
+++ Expression Tree/MainWindow.xaml.cs
@@ -1,13 +1,26 @@
 ﻿
-using System.Windows;
+using Expression_Tree.ViewModels;
 
 namespace Expression_Tree {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow {
         public MainWindow() {
+
+            DataContext = new ExpressionVisualizerViewModel();
             InitializeComponent();
+
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
         }
+
+        private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            if (e.PropertyName == "Expression") {
+                Nodes.Items.Clear();
+                Nodes.Items.Add(ViewModel.expressionTree.Root);
+            }
+        }
+
+        ExpressionVisualizerViewModel ViewModel => DataContext as ExpressionVisualizerViewModel;
     }
 }
---
+++ Expression Tree/ViewModels/ExpressionVisualizerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Expression_Tree.Tree;
+using Expression_Tree.ValueProvider;
+
+namespace Expression_Tree.ViewModels {
+    class ExpressionVisualizerViewModel {
+        public ExpressionVisualizerViewModel() {
+            var variables = new Dictionary<string, string> { { "one", "1" } };
+            expressionEvaluator = new ExpressionEvaluator(new Variables(variables), new Jurrasic(string.Empty));
+        }
+
+        public ExpressionTree expressionTree = new ExpressionTree();
+        private readonly ExpressionEvaluator expressionEvaluator;
+
+        private string expression;
+        public string Expression {
+            get => expression;
+            set {
+                if (expression == value)
+                    return;
+
+                expression = value;
+                expressionTree.Parse(expression);
+                expressionTree.Evaluate(expressionEvaluator);
+                RaisePropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
